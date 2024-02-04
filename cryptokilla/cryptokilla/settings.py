ccd0f23a5ea6387e328054c632bb96b1b9ab8104
Nabil-Mabrouk <nabil.mabrouk.ai@gmail.com>
2024-02-05 00:06:10 +0100

    Dockerized first version

--- cryptokilla/cryptokilla/settings.py
+++ cryptokilla/cryptokilla/settings.py
@@ -36,7 +36,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 #DEBUG = os.getenv('DEBUG') == 'True'
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env('DEBUG')
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'cryptokilla.cryptazy.com']
 
 
 # Application definition
